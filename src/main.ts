d57dd967cd43f38ea8bddb53419ea3095bcdb93c
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2024-07-24 08:44:03 +0300

    init website

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.scss'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main>
+    <header class="container">
+      <hgroup>
+        <h1>FixIt Mobile</h1>
+        <p>Coming soon ...</p>
+      </hgroup>
+    </header>
+  </main>
+`
